39457facd7a88988516c45ac72d9c6480ba805cd
Alpha1Dash1 <flalpha1dash1@gmail.com>
2023-03-11 16:19:26 +0500

    1st Module END

--- js/animations.js
+++ js/animations.js
@@ -42,3 +42,20 @@ function increaseNumberAnimationStep (i, element, endNumber) {
 	document.querySelector('#form form').removeChild(otherInput);
 	}
   });
+
+  function updateScroll() {
+	if (window.scrollY > 0) {
+	  document.querySelector('header').classList.add('header__scrolled');
+	} else {
+	  document.querySelector('header').classList.remove('header__scrolled');
+	}
+   
+	let windowBottomPosition = window.scrollY + window.innerHeight;
+	let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+	if (windowBottomPosition >= countElementPosition && !animationInited) {
+	  animationInited = true;
+	  initIncreaseNumberAnimation();
+	}
+  }
+   
+  window.addEventListener('scroll', updateScroll);
